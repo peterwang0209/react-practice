fc22c8f55c5ed7bd7825fb94c63b150a11e011d6
Peter <cloudwang0209@gmail.com>
2022-01-02 13:01:57 -0600

    start section 4

--- React_Basic/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ React_Basic/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
+import {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const [state, setstate] = useState(props.title);
+  const clickHandler = () => {
+    setstate('Updated');
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{state}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
